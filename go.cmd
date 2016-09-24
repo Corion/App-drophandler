9da22a687581aab9171f97daf1957a2eeac9c7ef
Max Maischein <corion@corion.net>
2016-09-24 14:12:17 +0200

    Add nicer text for the bookmarklet links

--- go.cmd
+++ go.cmd
@@ -3,5 +3,5 @@ call c:\strawberry-perl-5.20.1.1-x64\path.cmd
 
 :restart
 @rem call plackup -p 5001 -s Twiggy -I lib -a bin\app.pl
-call plackup -p 5001 -I lib -a bin\app.pl
+call plackup -l localhost:5001 -I lib -a bin\app.pl
 goto restart
