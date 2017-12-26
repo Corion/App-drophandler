034d01430d62abe50b5ad819a529202132c7797d
Max Maischein <corion@corion.net>
2017-12-26 21:41:54 +0100

    New Perl version

--- go.cmd
+++ go.cmd
@@ -1,7 +1,7 @@
 cd %~dp0
-call c:\strawberry-perl-5.20.1.1-x64\path.cmd
+call c:\strawberry-perl-5.26.0.1-64bit\path.cmd
 
 :restart
 @rem call plackup -p 5001 -s Twiggy -I lib -a bin\app.pl
-call plackup -l localhost:5001 -I lib -a bin\app.pl
+call plackup -l localhost:5002 -I lib -a bin\app.pl
 goto restart
